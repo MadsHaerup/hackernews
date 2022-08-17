c03243c6e003580aa1d52eee84fe8097a49ac989
Mads Haerup <506094@rts-365.dk>
2022-08-18 01:33:30 +0200

    fetching

--- hackernews/pages/index.tsx
+++ hackernews/pages/index.tsx
@@ -1,8 +1,13 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps} from 'next'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { Header } from '../components/header/header'
+import { PostItem } from '../components/posts/PostItem'
+import styles from '../styles/Home.module.scss'
+import PageData from '../types/posts'
 
-const Home: NextPage = () => {
+
+const Home = ({posts, karmaScore}:PageData) => {
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,10 +16,54 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Header />
+
       <main className={styles.main}>
+      {
+        posts.map((post, i)=>(
+          post.by === karmaScore[i].id ?  <PostItem key={post.id} post={post} authorKarma={karmaScore[i].karma} /> : null
+        ))
+      }
+
       </main>
     </div>
   )
+}
+
+// This gets called on every request
+export const getServerSideProps: GetServerSideProps = async ({ req, res }:any) => {
+  res.setHeader(
+    'Cache-Control',
+    'public, s-maxage=10, stale-while-revalidate=59'
+  );
+  // Fetch data from external API
+  const response = await fetch(`https://hacker-news.firebaseio.com/v0/topstories.json`);
+  const data = await response.json();
+
+  // create a shallow copy of the original array  - because the Array.sort method mutates the original array.
+  let randomPostID = [...data].sort(() => .5 - Math.random()).slice(0,10)
+
+  const jsonPosts = randomPostID.map(async(post) =>{
+    const response = await fetch(`https://hacker-news.firebaseio.com/v0/item/${post}.json`);
+    return await response.json();
+  });
+
+  const returnedData = await Promise.all(jsonPosts);
+  const ascSortedData = returnedData.sort((a, b) => a.score - b.score);
+
+  const authors = ascSortedData.map(async(authorName) =>{
+    const response = await fetch(`https://hacker-news.firebaseio.com/v0/user/${authorName.by}.json`);
+    return await response.json();
+  });
+
+  const returnedAuthors = await Promise.all(authors);
+
+  // Pass data to the page via props
+    return { props: { 
+      posts: JSON.parse(JSON.stringify(ascSortedData)),
+      karmaScore: JSON.parse(JSON.stringify(returnedAuthors))
+    } 
+  }
 }
 
 export default Home
